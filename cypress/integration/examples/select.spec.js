27cfcd4c59e5d971f735424d21a18ac83f425d8c
Noah Gelman <noah.gelman@adhocteam.us>
2021-03-02 23:18:35 -0800

    initial commit

---
+++ cypress/integration/examples/select.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+context('Keyboard Navigation', () => {
+  it('navigate', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get("body").realClick();
+    cy.realPress('Tab');
+    cy.realPress('ArrowDown');
+    cy.realPress('ArrowDown');
+    cy.realPress('ArrowDown');
+  });
+});
